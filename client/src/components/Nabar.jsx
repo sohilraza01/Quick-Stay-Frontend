7be67214659c58395913ab1a94a9a403431751f3
sohilraza01 <rsohil204@gmail.com>
2025-12-06 09:10:13 +0530

    admin-dashboard-completed

--- client/src/components/Nabar.jsx
+++ client/src/components/Nabar.jsx
@@ -11,7 +11,7 @@ const BookIcon = () => (
 
 const Navbar = () => {
     const navLinks = [
-        { name: 'Home', path: '/' },
+        { name: 'Sohil', path: '/' },
         { name: 'Hotel', path: '/rooms' },
         { name: 'Experience', path: '/' },
         { name: 'About', path: '/' },
